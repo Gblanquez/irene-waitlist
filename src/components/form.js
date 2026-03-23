9aa94978d8cfa90023ab60f6fa4a717be77b153c
Gblanquez <hellogabrielblanquez@gmail.com>
2026-03-23 05:12:41 +0100

    form update

--- src/components/form.js
+++ src/components/form.js
@@ -1,12 +1,13 @@
+import Lenis from 'lenis'
 import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { lenis } from "./scroll.js";
-gsap.registerPlugin(ScrollTrigger);
 
 export default function formAnimation() {
   const formHolder = document.querySelector('.form-holder')
   const formContainer = document.querySelector('.form-container')
   const ctas = document.querySelectorAll('.cta')
+
+  let formLenis = null
 
   gsap.set(formContainer, { y: '110%' })
 
@@ -14,7 +15,18 @@ export default function formAnimation() {
     cta.addEventListener('click', () => {
       lenis.stop()
       formHolder.classList.add('flex')
-      gsap.to(formContainer, { y: '0%', duration: 1.1, ease: 'power3.out' })
+
+      gsap.to(formContainer, {
+        y: '0%', duration: 1.1, ease: 'power3.out',
+        onComplete: () => {
+          formLenis = new Lenis({
+            wrapper: formHolder,
+            content: formHolder.children[0],
+            overscroll: false,
+          })
+          gsap.ticker.add((time) => formLenis?.raf(time * 1000))
+        }
+      })
     })
   })
 }
